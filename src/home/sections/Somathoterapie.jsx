c95bd771c1568988327b75f5c398b0a4ce670130
marwa <mrekik@partner.auchan.fr>
2020-10-04 19:31:46 +0200

    somatho

--- src/home/sections/Somathoterapie.jsx
+++ src/home/sections/Somathoterapie.jsx
@@ -11,9 +11,9 @@ const useStyles = makeStyles(({ palette, ...theme }) => ({
   },
 
   button: {
-    margin: "0 1rem 1rem 0 !important",
+    marginTop: "1rem !important",
     padding: "1rem 2rem 1rem 2rem",
-    fontSize: 10,
+    fontSize: 12,
     background: `${palette.second} !important`,
     color: "white",
     fontFamily: "LEMON MILK",
@@ -24,32 +24,29 @@ const useStyles = makeStyles(({ palette, ...theme }) => ({
     textAlign: "center"
 },
 
-  center: {
-   justifyContent: "center",
-   alignItems: "center",
-   height: "100px",
-   fontSize: "14px"
-  },
-
   p: {
     fontFamily: "Lato",
     fontStyle: "normal",
     fontWeight: "normal",
-    fontSize: "18px",
+    fontSize: "20px",
     lineHeight: "30px",
+    padding: "1rem 1rem 1rem 1rem",
+    alignItems: "center",
     textAlign: "left",
-    padding: "1rem 1rem 1rem 1rem"
+    paddingTop: "8rem",
+    maxWidth: "400px",
+    margin: "0 auto"
   },
 
   h1: {
     fontFamily: "LEMON MILK",
     fontStyle: "normal",
     fontWeight: "600",
-    fontSize: "26px",
+    fontSize: "32px",
     lineHeight: "21px",
-    alignItems: "center",
     textAlign: "center",
-    color: "#384C5A"
+    color: "#384C5A",
+    paddingTop: "2rem"
   },
 
 }));
@@ -64,7 +61,7 @@ const Somathoterapie = () => {
 
       <div className="container text-center">
      
-        <Grid container lg={12} md={12} sm={12} xs={12} spacing={0} justify="center" alignItems="center">
+        <Grid container lg={12} md={12} sm={12} xs={12} justifyContent="flex-start" alignItems="flex-start">
 
             <Grid lg={6} md={6} sm={6} xs={12}>
               <div
@@ -85,14 +82,21 @@ const Somathoterapie = () => {
             </Grid>
 
 
-            <Grid lg={6} md={6} sm={6} xs={12}>
+            <Grid lg={6} md={6} sm={6} xs={12} >
               
-            <h1 className={classes.h1}>Somatothérapie</h1>
+            <h1 className={classes.h1} >Somatothérapie</h1>
 
-            <p className={classes.p}>C’est une pratique qui instaure une relation d’aide par un toucher psycho-corporel.
+            <p className={classes.p} >
+            
+            C’est une pratique qui instaure une relation d’aide par un toucher psycho-corporel.
+
+            <br></br>
+            <br></br>
 
             Le praticien prend en compte vos ressentis corporels et permet de replacer 
-            votre corps au centre de votre attention. Cette approche vous invite à entrer en contact avec ce que vous ressentez, 
+            votre corps au centre de votre attention.
+            
+            Cette approche vous invite à entrer en contact avec ce que vous ressentez, 
             avec votre vécu corporel et intérieur de la situation que vous vivez.
 
             </p>
